791470e74c0250770c897bab8455e8e5fc97563d
Manish <mk925212@gmail.com>
2021-09-30 00:52:16 +0530

    Library Management Program

---
+++ library.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+struct student
+{
+    char name[25];
+    int id;
+    int rollno;
+};
+
+
+struct Book
+{
+    char title[20];
+    char author[20];
+    int bookID;
+    int page;
+    float price;
+};
+
+int main()
+{
+    printf("* * * * * Library * * * * *\n");
+    return 0;
+}
