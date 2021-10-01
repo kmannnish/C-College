5e0190126df6aaf7b53c25c909598e0e25f8c510
Manish <mk925212@gmail.com>
2021-10-02 00:37:05 +0530

    Library Funtion make

--- library.c
+++ library.c
@@ -19,6 +19,48 @@ struct Book
 
 int main()
 {
+    int a;
     printf("* * * * * Library * * * * *\n");
+    printf("Type Number You want to Perform");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+    case 1:
+        question (1);
+        break;
+    
+    case 2:
+        question (2);
+        break;
+    
+    case 3:
+        question (3);
+        break;
+    
+    case 4:
+        question (4);
+        break;
+    }
     return 0;
+}
+
+void question (int a)
+{
+    printf("Question 1");
+}
+
+void question (int a)
+{
+    printf("Question 2");
+}
+
+void question (int a)
+{
+    printf("Question 3");
+}
+
+void question (int a)
+{
+    printf("Question 4");
 }
